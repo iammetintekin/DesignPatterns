8e0cce47f0d2f813b11781c1497e2980f53af869
Metin Tekin <45108084+iammetintekin@users.noreply.github.com>
2023-10-13 01:29:46 +0300

    single responsibility

---
+++ SOLID/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SOLID;
+
+Console.WriteLine("-- Solid Prensipleri --");
+SingleResponsibility singleResponsibility = new SingleResponsibility();
+singleResponsibility.Run();
---
+++ SOLID/SingleResponsibility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    /// <summary>
+    /// Bir classın değiştirilmek için en az bir sebebi olmalı
+    /// </summary>
+    internal class SingleResponsibility
+    {
+        internal void Run()
+        {
+            Journal journal = new Journal();
+            Console.WriteLine(journal);
+            FileOperations fileOperations = new FileOperations();
+            fileOperations.SaveToFile(journal, "Temp_File.txt");
+        }
+        // Günlük ile ilgili gerekli işlemler yapılıyor.
+        // Fakat biz bunları dosyaya aktarmak istiyoruz diyelim.
+        // Bu işlemi yapmak için birden çok yöntem var fakat sorun burada journal sınıfına birden çok 
+        // iş yüklemek. 
+        // Şimdi, journal sınıfını değiştirmek için birden çok sebebimiz var. 
+        // Bu iş yükünü farklı bir classa dökeceğiz.
+        class Journal
+        {
+            public Journal()
+            {
+                // temp records
+                AddEntry("I cried today");
+                AddEntry("I ate bugs");
+            }
+            private readonly List<string> journals = new List<string>();
+
+            private static int Count = 0;
+            public int AddEntry(string text)
+            {
+                journals.Add($"{++Count} : {text}");
+                return Count;
+                //Memento pattern, bir nesnenin önceki durumunu, uygulama detaylarını açığa
+                //çıkartmadan kaydetmeniz ve geri getirmenizi sağlayan bir tasarım
+                //desenidir.
+            }
+            public void Remove(int index)
+            {
+                journals.RemoveAt(index);
+            }
+            public override string ToString()
+            {
+                return string.Join(Environment.NewLine, journals);
+            }
+        }
+        class FileOperations
+        {
+            public void SaveToFile(Journal journal, string fileName)
+            {
+                Console.WriteLine($"{fileName} created.");
+            }
+        }
+    }
+}
